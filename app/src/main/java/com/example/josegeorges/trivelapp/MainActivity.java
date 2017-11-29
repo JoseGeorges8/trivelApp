f829c1fb95e519f069d0284c129745c4e9f6eaec
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2017-11-29 09:11:28 -0500

    #3 added the items for the drawer menu, with icons.

--- app/src/main/java/com/example/josegeorges/trivelapp/MainActivity.java
+++ app/src/main/java/com/example/josegeorges/trivelapp/MainActivity.java
@@ -80,17 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_packages) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_wish_list) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_about_us) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_credits) {
 
         }
 
