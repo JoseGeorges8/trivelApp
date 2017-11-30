489fbd03fd3e54822130fb3923bda53bfab15fad
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2017-11-30 14:22:32 -0500

    #2 Just connected the PackagesFragment to its according nav item, also changed the colors of the app to test.

--- app/src/main/java/com/example/josegeorges/trivelapp/MainActivity.java
+++ app/src/main/java/com/example/josegeorges/trivelapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.josegeorges.trivelapp;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,7 +17,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        PackagesFragment.OnFragmentInteractionListener{
+
+    FragmentManager fm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,9 +85,11 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        fm = getSupportFragmentManager();
         if (id == R.id.nav_packages) {
-
+            FragmentTransaction transaction = fm.beginTransaction();
+            transaction.replace(R.id.content, new PackagesFragment());
+            transaction.commit();
         } else if (id == R.id.nav_wish_list) {
 
         } else if (id == R.id.nav_about_us) {
@@ -93,5 +101,10 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
