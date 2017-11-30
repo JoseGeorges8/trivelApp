5609dea6d6277b9d32ac4a665605597818f1f903
Nicholas Stajduhar <nicholas.stajduhar01@stclairconnect.ca>
2017-11-30 12:16:10 -0500

    #1 Designed basic look for the Trip View page.
    * Made temporary connection to the View from the Package tab in the Drawer (*Will be removed and moved in a future commit)
    
    * Added a tab host to allow for a cleaner design and more screen availability

--- app/src/main/java/com/example/josegeorges/trivelapp/MainActivity.java
+++ app/src/main/java/com/example/josegeorges/trivelapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.josegeorges.trivelapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,7 +18,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        tripviewFragment.OnFragmentInteractionListener{
+
+    FragmentManager fm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,8 +87,12 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_packages) {
+        fm = getSupportFragmentManager();
 
+        if (id == R.id.nav_packages) {
+            FragmentTransaction transaction = fm.beginTransaction();
+            transaction.replace(R.id.content, new tripviewFragment());
+            transaction.commit();
         } else if (id == R.id.nav_wish_list) {
 
         } else if (id == R.id.nav_about_us) {
@@ -93,5 +104,10 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
