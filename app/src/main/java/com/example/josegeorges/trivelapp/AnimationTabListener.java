7c99d9293b63bcc78b3734a9ded5c7debc74691d
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2017-12-14 08:22:17 -0500

    #5 finishing setting up the custom class for the tab animations and added the listener to the TabHost. Looks nice and smooth

--- app/src/main/java/com/example/josegeorges/trivelapp/AnimationTabListener.java
+++ app/src/main/java/com/example/josegeorges/trivelapp/AnimationTabListener.java
@@ -2,6 +2,9 @@ package com.example.josegeorges.trivelapp;
 
 import android.content.Context;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.TabHost;
 
 /**
@@ -12,8 +15,10 @@ import android.widget.TabHost;
 
 public class AnimationTabListener implements TabHost.OnTabChangeListener{
 
-    private Context context;
     private static final int ANIMATION_TIME = 240;
+
+    //needed properties
+    private Context context;
     private TabHost tabHost;
     private View previousView;
     private View currentView;
@@ -27,7 +32,77 @@ public class AnimationTabListener implements TabHost.OnTabChangeListener{
 
     @Override
     public void onTabChanged(String s) {
+        //Setting animation depending on which tab we are going and coming from
+        currentView = tabHost.getCurrentView();
+        if (tabHost.getCurrentTab() > currentTab) {
+            previousView.setAnimation(toLeftAnimation());
+            currentView.setAnimation(fromRightAnimation());
+        }else{
+            previousView.setAnimation(toRightAnimation());
+            currentView.setAnimation(fromLeftAnimation());
+        }
+        previousView = currentView;
+        currentTab = tabHost.getCurrentTab();
+    }
 
 
+
+    //Animations
+    private Animation fromLeftAnimation() {
+        Animation fromLeft = new TranslateAnimation(Animation.RELATIVE_TO_PARENT,
+                -1.0f,
+                Animation.RELATIVE_TO_PARENT,
+                0.0f,
+                Animation.RELATIVE_TO_PARENT,
+                0.0f,
+                Animation.RELATIVE_TO_PARENT,
+                0.0f);
+        return setProperties(fromLeft);
+    }
+
+
+
+    private Animation toRightAnimation() {
+        Animation toRight = new TranslateAnimation(Animation.RELATIVE_TO_PARENT,
+                0.0f,
+                Animation.RELATIVE_TO_PARENT,
+                1.0f,
+                Animation.RELATIVE_TO_PARENT,
+                0.0f,
+                Animation.RELATIVE_TO_PARENT,
+                0.0f);
+        return setProperties(toRight);
+    }
+
+    private Animation fromRightAnimation() {
+        Animation fromRight = new TranslateAnimation(Animation.RELATIVE_TO_PARENT,
+                1.0f,
+                Animation.RELATIVE_TO_PARENT,
+                0.0f,
+                Animation.RELATIVE_TO_PARENT,
+                0.0f,
+                Animation.RELATIVE_TO_PARENT,
+                0.0f);
+        return setProperties(fromRight);
+    }
+
+    private Animation toLeftAnimation() {
+        Animation toLeft = new TranslateAnimation(Animation.RELATIVE_TO_PARENT,
+                0.0f,
+                Animation.RELATIVE_TO_PARENT,
+                -1.0f,
+                Animation.RELATIVE_TO_PARENT,
+                0.0f,
+                Animation.RELATIVE_TO_PARENT,
+                0.0f);
+        return setProperties(toLeft);
+
+    }
+
+    //setting final common properties to the animations
+    private Animation setProperties(Animation animation) {
+        animation.setDuration(ANIMATION_TIME);
+        animation.setInterpolator(new AccelerateInterpolator());
+        return animation;
     }
 }
