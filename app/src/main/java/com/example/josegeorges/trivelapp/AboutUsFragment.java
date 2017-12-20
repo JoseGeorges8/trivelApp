c87be4b2022d365bfc24cced6982cdfc8cca0c7b
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2017-12-20 08:00:34 -0500

    #27 Created an AboutUsFragment

---
+++ app/src/main/java/com/example/josegeorges/trivelapp/AboutUsFragment.java
@@ -0,0 +1,47 @@
+package com.example.josegeorges.trivelapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AboutUsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AboutUsFragment extends Fragment {
+
+
+
+    public AboutUsFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static AboutUsFragment newInstance(String param1, String param2) {
+        AboutUsFragment fragment = new AboutUsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_us2, container, false);
+    }
+
+}
