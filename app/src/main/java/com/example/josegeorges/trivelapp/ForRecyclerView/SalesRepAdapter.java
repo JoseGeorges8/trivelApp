d0e7f488cb2e34136c988e0cd89906b8fbeca5bc
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2017-12-20 09:43:35 -0500

    #27 declared an adapter for th salesrep RecyclerView and a SalesRep object

---
+++ app/src/main/java/com/example/josegeorges/trivelapp/ForRecyclerView/SalesRepAdapter.java
@@ -0,0 +1,31 @@
+package com.example.josegeorges.trivelapp.ForRecyclerView;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.example.josegeorges.trivelapp.SalesRep;
+
+/**
+ * Created by josegeorges on 2017-12-20.
+ */
+
+public class SalesRepAdapter extends RecyclerView.Adapter<SalesRepViewHolder> {
+
+    
+
+
+    @Override
+    public SalesRepViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(SalesRepViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/josegeorges/trivelapp/SalesRep.java
@@ -0,0 +1,11 @@
+package com.example.josegeorges.trivelapp;
+
+/**
+ * Created by josegeorges on 2017-12-20.
+ */
+
+public class SalesRep {
+
+
+
+}
