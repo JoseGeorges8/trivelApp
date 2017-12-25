914e4bbf9353c0ddd25b121ed183f6858b860290
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2017-12-25 05:38:30 -0500

    #45 the last item should not hve the divider, so I am using a solution I found where I extend the item decoration and override the onDraw method

---
+++ app/src/main/java/com/example/josegeorges/trivelapp/RecyclerViewDivider.java
@@ -0,0 +1,55 @@
+package com.example.josegeorges.trivelapp;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by josegeorges on 2017-12-25.
+ */
+
+public class RecyclerViewDivider extends RecyclerView.ItemDecoration{
+    
+    private Drawable divider;
+    private static final int[] ATTRS = new int[]{android.R.attr.listDivider};
+
+    /**
+     * Default divider will be used
+     */
+    public RecyclerViewDivider(Context context) {
+        final TypedArray styledAttributes = context.obtainStyledAttributes(ATTRS);
+        divider = styledAttributes.getDrawable(0);
+        styledAttributes.recycle();
+    }
+
+    /**
+     * Custom divider will be used
+     */
+    public RecyclerViewDivider(Context context, int resId) {
+        divider = ContextCompat.getDrawable(context, resId);
+    }
+
+    @Override
+    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = parent.getChildAt(i);
+
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+            int top = child.getBottom() + params.bottomMargin;
+            int bottom = top + divider.getIntrinsicHeight();
+
+            divider.setBounds(left, top, right, bottom);
+            divider.draw(c);
+        }
+    }
+}
+
